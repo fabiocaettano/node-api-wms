08f782ff291c0828f9b726d3da1c39565f46aa40
fabiocaettano <fabio.caettano74@gmail.com>
2022-04-29 15:17:54 -0300

    configurado variaveis de ambiente; arquivo .env; biblioteca dotenv

--- node/dbServer.js
+++ node/dbServer.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const app = express()
 const mysql = require("mysql")
+require("dotenv").config()
+
 const db = mysql.createPool({
    connectionLimit: 100,
-   host: "db",       //This is your localhost IP
-   user: "root",         // "newuser" created in Step 1(e)
-   password: "root",  // password for the new user
-   database: "nodedb",      // Database name
-   port: "3306"             // port name, "3306" by default
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_DATABASE,
+   port: process.env.DB_PORT
 })
 db.getConnection( (err, connection)=> {
    if (err) throw (err)
